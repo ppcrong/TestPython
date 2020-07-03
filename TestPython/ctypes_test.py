30a79355e5ed4c7fc64a3fa412c11d844ca4350c
ppcrong <ppcrong@gmail.com>
2020-07-03 19:03:48 +0800

    [ctypes] add more assignment method

--- TestPython/ctypes_test.py
+++ TestPython/ctypes_test.py
@@ -1,7 +1,7 @@
 from ctypes import *
 
 # visual studio
-#dll_c = CDLL('dll/ctypes_dll.dll')
+# dll_c = CDLL('dll/ctypes_dll.dll')
 # vscode
 dll_c = CDLL('dll/libctypes_dll.dll')
 
@@ -14,6 +14,7 @@ print('c_value: {}'.format(c_value))
 # read data of byte array
 import numpy as np
 
+# c_buf_p assign method 1
 buf = np.arange(1, 17, dtype=np.byte)
 c_buf_p = (c_byte * len(buf))(*buf)
 dll_c.test_buf(c_buf_p)
@@ -21,6 +22,16 @@ buf = list(c_buf_p)
 print('buf: {}'.format(buf))
 buf = np.ctypeslib.as_array(c_buf_p, np.int8)
 print('buf: {}'.format(buf))
+
+# c_buf_p assign method 2
+c_buf_p = (c_byte * 16)()
+dll_c.test_buf(c_buf_p)
+buf = list(c_buf_p)
+print('buf: {}'.format(buf))
+buf = cast(c_buf_p, POINTER(c_byte * 16)).contents
+print('buf: {}'.format(buf))
+for b in buf:
+    print(b)
 
 
 # read data of bounding_box_s
@@ -34,18 +45,35 @@ class bounding_box_s(Structure):
         ('class_num', c_int32)
     ]
 
+    def __repr__(self):
+        ret_str = ''
+        for field in self._fields_:
+            ret_str = '\n'.join((ret_str, '\t{}: {}'.format(field[0], getattr(self, field[0]))))
+        return ret_str
+
 
 box1 = bounding_box_s(x1=0, y1=0, x2=0, y2=0, score=0, class_num=0)
 box2 = bounding_box_s(x1=0, y1=0, x2=0, y2=0, score=0, class_num=0)
 boxes = [box1, box2]
-print('boxes: {}'.format(boxes))
+print('\n===1==={}'.format(boxes))
+
+# c_boxes_p assign method 1
 c_boxes_p = (bounding_box_s * len(boxes))(*boxes)
 c_boxes_pp = pointer(c_boxes_p)
 ret = dll_c.test_struct_array(byref(c_boxes_pp), len(boxes))
 print('ret: {}'.format(ret))
 boxes = list(c_boxes_p)
-print('boxes: {}'.format(boxes))
-print('boxes[0]: x1:{:.1f},y1:{:.1f}'.format(boxes[0].x1, boxes[0].y1))
-print('boxes[0]: x2:{:.1f},y2:{:.1f}'.format(boxes[0].x2, boxes[0].y2))
-print('boxes[1]: x1:{:.1f},y1:{:.1f}'.format(boxes[1].x1, boxes[1].y1))
-print('boxes[1]: x2:{:.1f},y2:{:.1f}'.format(boxes[1].x2, boxes[1].y2))
+print('\n===2==={}'.format(boxes))
+
+# c_boxes_p assign method 2
+boxes = [box1, box2]
+print('\n===3==={}'.format(boxes))
+c_boxes_p = (bounding_box_s * 2)()
+c_boxes_pp = pointer(c_boxes_p)
+ret = dll_c.test_struct_array(byref(c_boxes_pp), len(boxes))
+print('ret: {}'.format(ret))
+boxes = list(c_boxes_p)
+print('\n===4==={}'.format(boxes))
+boxes = cast(c_boxes_p, POINTER(bounding_box_s * 2)).contents
+for b in boxes:
+    print('\n===5==={}'.format(b))
